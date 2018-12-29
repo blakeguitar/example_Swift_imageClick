1b512bdd951e3cf947f97cac7476031e2cf0befd
BlakeCMartin <blakeguitar6@gmail.com>
2018-12-29 17:53:07 -0600

    add border and center image

--- exampleImageClick/ViewController.swift
+++ exampleImageClick/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageSelect: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageSelect.layer.borderWidth = 2 // as you wish
     }
 
 
