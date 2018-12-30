01e96b9c4288e3f96c31d737e0129a48183219fe
BlakeCMartin <blakeguitar6@gmail.com>
2018-12-29 19:32:43 -0600

    add click gesture

--- exampleImageClick/ViewController.swift
+++ exampleImageClick/ViewController.swift
@@ -10,14 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageSelect: UIImageView!
     
+    //MARK: Private Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    //MARK: Internal Properties
+    
+    // Initialize
+    func initialize() {
+        
+        addTapGesture()
+        
+        imageView.layer.borderWidth = 2
+        
+    }
+    
+
+    // ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        imageSelect.layer.borderWidth = 2 // as you wish
+        
+        initialize()
     }
-
+    
+    // Add Tap Gesture
+    func addTapGesture() {
+        
+        // create tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTapped(gesture:)))
+        
+        // add gesture to the imageView
+        imageView.addGestureRecognizer(tapGesture)
+        
+        // make sure imageView can be interacted with by user
+        imageView.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func imageTapped(gesture: UIGestureRecognizer) {
+        // if the tapped view is a UIImageView then set it to imageview
+        if (gesture.view as? UIImageView) != nil {
+            print("Image Tapped")
+            //Here you can initiate your new ViewController
+            
+        }
+    }
+    
 
 }
 
